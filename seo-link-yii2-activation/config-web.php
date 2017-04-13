386f7415eff716acf8458b020712d9f509427786
Evgeniy Lucovskiy <evgendpua@gmail.com>
2017-04-13 16:06:41 +0300

    2

---
+++ seo-link-yii2-activation/config-web.php
@@ -0,0 +1,10 @@
+<?php
+
+'components' => [
+	// ... какие-то опции
+	'urlManager' => [
+		'enablePrettyUrl' => true, // активирует формат ЧПУ.
+		'showScriptName' => false,  // убирает имя входного скрипта
+	],
+	// какие-то опции ...
+],
